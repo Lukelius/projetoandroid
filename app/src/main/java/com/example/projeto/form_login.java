d8a263e5bbfbdeef729f3ae6809584006ecaa5b8
lucas <lucasmessiasg@gmail.com>
2023-11-06 16:09:38 -0300

    projeto em andamento

--- app/src/main/java/com/example/projeto/form_login.java
+++ app/src/main/java/com/example/projeto/form_login.java
@@ -2,13 +2,38 @@ package com.example.projeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class form_login extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-    }
+
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(form_login.this,form_cadastro.class);
+                startActivity(intent);
+            }
+        });
+
+   }
+
+   private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.cadastrar);
+
+   }
 }
